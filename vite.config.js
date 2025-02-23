3ae26607f52841f806249c341b6ff1f6a84f2fc5
David Brown <2047439+dugbus@users.noreply.github.com>
2025-02-23 19:19:54 +0000

    New pages and moved things about

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        wordleGolf: 'projects/wordle-golf/index.html',
+        code: 'code/scrypt/index.html',
+        wordleGolf: 'text/wordle-golf/index.html',
         canvasTestCard: 'canvas/test-card/index.html',
-        canvasWipeout: 'p5/wipeout/index.html',
-        perlinContours: 'p5/perlin-contours/index.html',
+        p5Wipeout: 'p5/wipeout/index.html',
+        p5PerlinContours: 'p5/perlin-contours/index.html',
+        p5ChaosCircles: 'p5/chaos-circles/index.html',
+        p5Tadpoles: 'p5/tadpoles/index.html',
       }
     }
   }
